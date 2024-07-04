f87c773e77d5ddd24bf026c15081802ba8a75a41
dineshGitHub93 <dineshspachaiyappan93@gmail.com>
2024-07-04 23:40:33 +0530

    String practice level1

--- stringinjs.js
+++ stringinjs.js
@@ -2,3 +2,11 @@
 
 myVariables = "Javascript Learning";
 console.log(myVariables);
+
+//String methods:
+//To find length of variables:
+console.log("Length of variable : "+myVariables.length);
+
+//To find char using charAt()
+console.log("Character of index 5 :"+ myVariables.charAt(5));
+
